5a8d317715743773e731aa0513221bc731f18042
Caio Lucas <caio.lps@aluno.ufop.edu.br>
2023-08-01 10:51:06 -0300

    feat: generate bin in main

--- src/main.c
+++ src/main.c
@@ -1,8 +1,38 @@
+#include "consts.h"
 #include "sort/merge-sort.h"
+#include "sort/tapes.h"
+#include "utils/file-handler.h"
+#include "utils/status-messages.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-  printf("Hello, world!\n");
+int main(int argc, char **argv) {
+  // Tape tape;
+
+  if (argc < 4) {
+    error_msg("Missing arguments\n");
+    info_msg("Example: ./exe <method> <quantity> <situation>");
+    return EXIT_FAILURE;
+  }
+
+  Input input;
+  input.method = (Method)atoi(argv[1]);
+  input.quantity = atoi(argv[2]);
+  input.situation = (Situation)atoi(argv[3]);
+
+  txt_to_bin("provao.txt", "tmp/provao.bin", input.quantity);
+
+  switch (input.method) {
+  case INTERNAL_INTERCALATION:
+    // TODO;
+    break;
+  case EXTERNAL_INTERCALATION:
+    // TODO;
+    break;
+  case QUICK_SORT:
+    // TODO;
+    break;
+  }
+
   return EXIT_SUCCESS;
 }
