c7bb28dabf126a059948aea19a1262900ffb54db
Caio Lucas <caio.lps@aluno.ufop.edu.br>
2023-08-04 21:06:18 -0300

    fix: spelling issue

--- src/consts.h
+++ src/consts.h
@@ -39,10 +39,11 @@ typedef struct Input {
   Situation situation;
 } Input;
 
-// Perfomance struct to measure reads, comparisons and execution time in a
+// Performance struct to measure reads, comparisons and execution time in a
 // function
-typedef struct Perfomance {
+typedef struct Performance {
   size_t reads_count;
+  size_t writes_count;
   size_t comparisons_count;
   double execution_time;
-} Perfomance;
+} Performance;
